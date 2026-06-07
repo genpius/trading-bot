6df9b8c946ac0552646d0d72ff1db8a527dd586e
Emeka Oramunwa <39782083+genpius@users.noreply.github.com>
2026-06-07 15:42:08 +0100

    Update real_trade_bot.py

--- real_trade_bot.py
+++ real_trade_bot.py
@@ -10,7 +10,7 @@ CHAT_ID = "7120687986"
 
 # ===== POCKET OPTION CONFIGURATION =====
 # 🔻🔻🔻 REPLACE THIS WITH YOUR ACTUAL SSID 🔻🔻🔻
-SSID = '42["auth",{"session":"a%3A2%3A%7Bs%3A6%3A%22key_id%22%3Bs%3A16%3A%220a25c12706a1a102%22%3Bs%3A7%3A%22user_id%22%3Bs%3A9%3A%22134215227%22%3B%7D","isDemo":1,"uid":"5c6a849a-1c1f-4b15-bfa7-ee3eb4052368","platform":2}]'
+SSID = '42["auth",{"session":"a%3A2%3A%7Bs%3A6%3A%22key_id%22%3Bs%3A16%3A%220a25c12706a1a102%22%3Bs%3A7%3A%22user_id%22%3Bs%3A9%3A%22134215227%22%3B%7D","isDemo":1,"uid":"f08ed2df-5554-4a85-8fc6-063c726d4b8c-p","platform":2}]'
 # 🔺🔺🔺 REPLACE THIS WITH YOUR ACTUAL SSID 🔺🔺🔺
 
 IS_DEMO = True
